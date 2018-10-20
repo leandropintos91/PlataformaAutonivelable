010477ceaa13588414419d640a96d0806cb1188a
silvina <silvinacace@gmail.com>
2018-10-20 13:26:12 -0300

    Archivo de Configuracion

---
+++ sistema_embebido/PlataformaAutonivelable/Config.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+#define motor1Pin1 4
+#define motor1Pin2 5
+#define motor2Pin1 6
+#define motor2Pin2 7
+#define motor3Pin1 8
+#define motor3Pin2 9
+#define pulsador1 10
+#define pulsador2 11
+#define pulsador3 12
+
